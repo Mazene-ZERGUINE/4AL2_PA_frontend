3fcbe9cc869fd448d8ce7a411c9097b04df7ae51
Mazene <mmazenezerguine@gmail.com>
2024-05-04 00:35:23 +0200

    setting routing

--- src/app/esgithub/esgithub.module.ts
+++ src/app/esgithub/esgithub.module.ts
@@ -2,9 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { EsgithubRoutingModule } from './esgithub-routing.module';
+import { CodingPageComponent } from './coding-page/coding-page.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, EsgithubRoutingModule],
+  declarations: [CodingPageComponent],
+  imports: [
+    CommonModule,
+    EsgithubRoutingModule,
+    MatIconModule,
+    MatInputModule,
+    MatSelectModule,
+    FormsModule,
+  ],
 })
 export class EsgithubModule {}
