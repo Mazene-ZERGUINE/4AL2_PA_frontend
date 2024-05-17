b9c0c2c2624d085fc815368ba8d69712ec86bf64
Mazene <mmazenezerguine@gmail.com>
2024-05-17 19:52:59 +0200

    appling auth guard on routes

--- src/app/esgithub/esgithub-routing.module.ts
+++ src/app/esgithub/esgithub-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { CodingPageComponent } from './coding-page/coding-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ProfileComponent } from './profile/profile.component';
+import { AuthGuard } from '../core/guards/auth.guard';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'auth' },
-  { path: 'coding', component: CodingPageComponent, canActivate: [] },
-  { path: 'home', component: HomePageComponent, canActivate: [] },
-  { path: 'profile', component: ProfileComponent, canActivate: [] },
+  { path: 'coding', component: CodingPageComponent, canActivate: [AuthGuard] },
+  { path: 'home', component: HomePageComponent, canActivate: [AuthGuard] },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
