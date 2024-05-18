95ae575e3ffc884d579be4283319ff6e0b9dc913
Mazene <mmazenezerguine@gmail.com>
2024-05-18 22:52:18 +0200

    fix interceptor

--- src/app/core/interceptors/request.interceptor.ts
+++ src/app/core/interceptors/request.interceptor.ts
@@ -13,11 +13,10 @@ import { NotifierService } from '../services/notifier.service';
 @Injectable()
 export class RequestInterceptor implements HttpInterceptor {
   constructor(private notifierService: NotifierService) {}
-
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
       catchError((error: HttpErrorResponse) => {
-        const errorMessage = error.error.error.error;
+        const errorMessage = error.error.error.message;
         this.notifierService.showError(errorMessage);
         return throwError(() => new Error(errorMessage));
       }),
