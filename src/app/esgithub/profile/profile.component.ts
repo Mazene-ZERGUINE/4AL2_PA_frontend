40503c8004813b3b63233263f252b443f4a9ab83
Mazene <mmazenezerguine@gmail.com>
2024-05-18 22:54:16 +0200

    user profile programs list / edit / password edit

--- src/app/esgithub/profile/profile.component.ts
+++ src/app/esgithub/profile/profile.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ProfileService } from './profile.service';
 import { AuthService } from '../../core/Auth/auth.service';
 import { Observable, switchMap } from 'rxjs';
 import { UserDataModel } from '../../core/models/user-data.model';
 import { ProgramModel } from '../../core/models/program.model';
+import { NotifierService } from '../../core/services/notifier.service';
 
 @Component({
   selector: 'app-profile',
@@ -11,24 +12,46 @@ import { ProgramModel } from '../../core/models/program.model';
   styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
+  @ViewChild('fileInput') fileInput!: ElementRef;
   userData$!: Observable<UserDataModel>;
   selectedMenuItem: string = 'programs';
   programsList$!: Observable<ProgramModel[]>;
+
   constructor(
     private readonly profileService: ProfileService,
     private readonly authService: AuthService,
+    private readonly notifier: NotifierService,
   ) {}
 
   ngOnInit() {
+    this.loadUserData();
+  }
+
+  loadUserData() {
     this.userData$ = this.authService.getUserData();
     this.programsList$ = this.userData$.pipe(
       switchMap((userData) => this.profileService.getUserPrograms(userData.userId)),
     );
-
-    this.programsList$.subscribe((res) => console.log(res));
+    this.userData$.subscribe((res) => console.log(res));
   }
 
   onMenuItemClick(item: string) {
     this.selectedMenuItem = item;
+  }
+
+  onProfileUpdated() {
+    this.loadUserData();
+  }
+
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0];
+      this.profileService.uploadAvatar(file).subscribe(() => {
+        this.onProfileUpdated();
+        this.loadUserData();
+        this.notifier.showSuccess('profile image updated');
+      });
+    }
   }
 }
