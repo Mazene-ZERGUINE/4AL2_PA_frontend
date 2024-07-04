76376e4a4c7ad4ec668d9214d681a4e433d793cb
Mazene <mmazenezerguine@gmail.com>
2024-07-04 05:46:10 +0200

    adding pipelines component

---
+++ src/app/esgithub/piplines/pipelines.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-piplines',
+  templateUrl: './pipelines.component.html',
+  styleUrls: ['./pipelines.component.scss'],
+})
+export class PipelinesComponent {}
