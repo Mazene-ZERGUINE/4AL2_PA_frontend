96f6920b71f1f34eb402e5167290197384a6a85f
Mazene <mmazenezerguine@gmail.com>
2024-07-15 20:15:55 +0200

    small fix pipelines

--- src/app/esgithub/piplines/pipelines.component.ts
+++ src/app/esgithub/piplines/pipelines.component.ts
@@ -15,6 +15,7 @@ import { ChangeDetectorRef } from '@angular/core';
 import { animate, state, style, transition, trigger } from '@angular/animations';
 import { CodingProcessorService } from '../coding-page/coding-processor.service';
 import { Router } from '@angular/router';
+import { environment } from '../../../environment/environment';
 
 @Component({
   selector: 'app-piplines',
@@ -223,9 +224,9 @@ export class PipelinesComponent implements OnInit, OnDestroy {
         .runPipeLinesWithFiles(formData)
         .pipe(
           takeUntil(this.componentDestroyer$),
-          tap((response: { success: boolean; outputFiles: string[]; error?: string }) => {
+          tap((response: { success: boolean; outputLinks: string[]; error?: string }) => {
             if (response.success) {
-              this.generatedFiles = response.outputFiles;
+              this.generatedFiles = response.outputLinks;
               this.notifierService.showSuccess('pipeline executed successfully.');
             } else {
               this.outputError = response.error;
@@ -248,7 +249,8 @@ export class PipelinesComponent implements OnInit, OnDestroy {
   }
 
   protected onGeneratedFileClick(filePath: string): void {
-    this.codeProcessorService.downloadOutputFile(filePath);
+    const url = environment.baseUrl.replace('/api/v1', '/') + filePath.trim();
+    this.codeProcessorService.downloadOutputFile(url);
   }
 
   protected getFileIcon(fileName: string): string {
