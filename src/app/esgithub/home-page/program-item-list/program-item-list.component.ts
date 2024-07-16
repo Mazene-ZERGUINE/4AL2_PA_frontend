74403f5e0e0fe0caef64efe05f2c23cad3b1f765
Zakarya Lahmadi <zakarya.lahmadi@affluences.com>
2024-07-17 01:36:14 +0200

    feat: fix bugss
    Co-authored-by: LoicZHU <57401386+LoicZHU@users.noreply.github.com>

--- src/app/esgithub/home-page/program-item-list/program-item-list.component.ts
+++ src/app/esgithub/home-page/program-item-list/program-item-list.component.ts
@@ -32,7 +32,7 @@ export class ProgramItemListComponent implements OnInit, OnDestroy {
   programCommentsCount$: Observable<number> | undefined;
   isProgramOwner: boolean = false;
   userReaction?: ReactionModel;
-  likes$: Observable<number> | undefined;
+  likes$!: Observable<number>;
   dislikes$!: Observable<number>;
 
   readonly ReactionsEnum = ReactionsEnum;
--- src/app/esgithub/profile/user-programs/user-programs.component.ts
+++ src/app/esgithub/profile/user-programs/user-programs.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ReactionsEnum } from 'src/app/shared/enums/reactions.enum';
 import { ProgramModel } from '../../../core/models/program.model';
+import { UserDataModel } from 'src/app/core/models/user-data.model';
 
 @Component({
   selector: 'app-user-programs',
@@ -10,7 +11,11 @@ import { ProgramModel } from '../../../core/models/program.model';
 export class UserProgramsComponent {
   @Input() programsList: ProgramModel[] | undefined;
 
+  @Input() currentUser: UserDataModel | undefined;
+
   @Input() profileOwner: boolean | undefined;
+
+  @Output() userProgramHasBeenLiked = new EventEmitter<void>();
 
   readonly ReactionsEnum = ReactionsEnum;
 }
